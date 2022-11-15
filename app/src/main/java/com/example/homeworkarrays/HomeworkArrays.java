8e5b4f8a859e6a7b4bc5afe81c668b8374f09b6d
BABASTAS728 <Aberezkin954@gmail.com>
2022-11-16 01:35:56 +0300

    task1

--- app/src/main/java/com/example/homeworkarrays/HomeworkArrays.java
+++ app/src/main/java/com/example/homeworkarrays/HomeworkArrays.java
@@ -1,7 +1,16 @@
 package com.example.homeworkarrays;
 
-public class HomeworkArrays {
-    public static void main(String[] args){
+import java.util.Arrays;
 
+public class HomeworkArrays {
+    public static void main(String[] args) {
+        nameArray();
+    }
+
+    public static void nameArray() {
+        String[] array = {"Dana", "Andrey", "Nikita", "Dima", "Vlad", "Dasha"};
+        System.out.println("Unsorted array: " + Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Sorted array: " + Arrays.toString(array));
     }
 }
