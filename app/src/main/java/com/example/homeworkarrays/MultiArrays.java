9398c8dec29d21577a3427ed58252c04e6b20510
BABASTAS728 <Aberezkin954@gmail.com>
2022-11-20 01:56:42 +0300

    task3 changes

--- app/src/main/java/com/example/homeworkarrays/MultiArrays.java
+++ app/src/main/java/com/example/homeworkarrays/MultiArrays.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class MultiArrays {
     public static void main(String[] args) {
-        // task1();
-        //task2();
+        task1();
+        task2();
         task3();
     }
 
