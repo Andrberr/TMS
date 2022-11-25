09cb206ae800b662907b2bcf529b438229153a02
BABASTAS728 <Aberezkin954@gmail.com>
2022-11-25 13:28:21 +0300

    simCards output change

--- app/src/main/java/homeworkoop/A1SimCard.java
+++ app/src/main/java/homeworkoop/A1SimCard.java
@@ -13,11 +13,13 @@ public final class A1SimCard extends SimCard {
     @Override
     public void makeCall(String simNum) {
         if (simNum.startsWith("33")) {
+            System.out.println("Звоню на номер " + simNum);
             if (this.getBalance() < 1) System.out.print("На балансе недостаточно средств. ");
             else {
                 this.setBalance(this.getBalance() - 1);
             }
         } else if (simNum.startsWith("29") || simNum.startsWith("44")) {
+            System.out.println("Звоню на номер " + simNum);
             if (this.getBalance() < 3) System.out.print("На балансе недостаточно средств. ");
             else {
                 this.setBalance(this.getBalance() - 3);
@@ -28,10 +30,12 @@ public final class A1SimCard extends SimCard {
     @Override
     public void receiveCall(String simNum) {
         if (simNum.startsWith("29") || simNum.startsWith("44")) {
+            System.out.println("Принимаю звонок с номера " + simNum);
             if (this.getBalance() < 1) System.out.print("На балансе недостаточно средств. ");
             else {
                 this.setBalance(this.getBalance() - 1);
             }
         } else if (!(simNum.startsWith("33"))) System.out.println("Неверная симкарта.");
+        else System.out.println("Принимаю звонок с номера " + simNum);
     }
 }
