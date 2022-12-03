071499fcb45976fc48cc9fd5cad402c8f583ab5e
BABASTAS728 <Aberezkin954@gmail.com>
2022-12-03 23:15:05 +0300

    exceptions homework changes

--- app/src/main/java/com/example/homeworkarrays/lesson9/practice/Except.java
+++ app/src/main/java/com/example/homeworkarrays/lesson9/practice/Except.java
@@ -3,14 +3,15 @@ package com.example.homeworkarrays.lesson9.practice;
 import java.util.Scanner;
 
 public class Except {
+    static final int UP_CASE_BEGIN = 65, UP_CASE_END = 90, LOW_CASE_BEGIN = 97, LOW_CASE_END = 122, NUM_BEGIN = 48, NUM_END = 57, UNDER_LINE = 95;
 
-    public static boolean check(String str) {
+    public static boolean checkForCorrect(String str) {
         boolean correct = true;
         if (str.length() >= 20) correct = false;
         if (correct) {
             for (int i = 0; i < str.length(); i++) {
                 char code = str.charAt(i);
-                if (!((code >= 65 && code <= 90) || (code >= 97 && code <= 122) || (code >= 48 && code <= 57) || code == 95)) {
+                if (!((code >= UP_CASE_BEGIN && code <= UP_CASE_END) || (code >= LOW_CASE_BEGIN && code <= LOW_CASE_END) || (code >= NUM_BEGIN && code <= NUM_END) || code == UNDER_LINE)) {
                     correct = false;
                     break;
                 }
@@ -19,9 +20,9 @@ public class Except {
         return correct;
     }
 
-    public static void test(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (!check(login)) throw new WrongLoginException("Wrong login");
-        if (!check(password) || !password.equals(confirmPassword))
+    public static void checkInput(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (!checkForCorrect(login)) throw new WrongLoginException("Wrong login");
+        if (!checkForCorrect(password) || !password.equals(confirmPassword))
             throw new WrongPasswordException("Wrong Password");
     }
 
@@ -34,11 +35,9 @@ public class Except {
             String password = in.nextLine();
             System.out.print("Введите повторно Пароль: ");
             String confirmPassword = in.nextLine();
-            test(login, password, confirmPassword);
-        } catch (WrongLoginException e1) {
-            System.out.println("Некорректный ввод логина.");
-        } catch (WrongPasswordException e2) {
-            System.out.println("Некорректный ввод пароля.");
+            checkInput(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
         }
     }
 }
