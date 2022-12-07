5e78279af7feffb05d42f81f39690f720cb8e949
BABASTAS728 <Aberezkin954@gmail.com>
2022-12-07 19:35:45 +0300

    methods access change

--- app/src/main/java/com/example/homeworkarrays/lesson10/homework/Document.java
+++ app/src/main/java/com/example/homeworkarrays/lesson10/homework/Document.java
@@ -8,27 +8,27 @@ public class Document {
         return false;
     }
 
-    public static void printTwoBlocksNumbers(String documentNum) {
+    private static void printTwoBlocksNumbers(String documentNum) {
         System.out.println(documentNum.substring(0, 4) + documentNum.substring(8, 13));
     }
 
-    public static void printDocumentNumReplaced(String documentNum) {
+    private static void printDocumentNumReplaced(String documentNum) {
         System.out.println(documentNum.replaceAll("[A-z]", "*"));
     }
 
-    public static void printDocumentLetters(String documentNum) {
+    private static void printDocumentLetters(String documentNum) {
         StringBuilder docNum = new StringBuilder();
         docNum.append(documentNum.substring(5, 8).toUpperCase()).append("/").append(documentNum.substring(14, 17).toUpperCase()).append("/").append(documentNum.substring(19, 20).toUpperCase()).append("/").append(documentNum.substring(21, 22).toUpperCase());
         System.out.println("Letters:" + docNum);
     }
 
-    public static void checkForABCSequence(String documentNum) {
+    private static void checkForABCSequence(String documentNum) {
         if (documentNum.contains("abc") || documentNum.contains("ABC"))
             System.out.println("Номер документа содержит последовательность abc");
         else System.out.println("Номер документа не содержит последовательность abc");
     }
 
-    public static void checkEndsNumber(String documentNum) {
+    private static void checkEndsNumber(String documentNum) {
         if (documentNum.endsWith("1a2b"))
             System.out.println("Номер документа заканчивается последовательностью 1a2b");
         else System.out.println("Номер документа не заканчивается последовательностью 1a2b");
