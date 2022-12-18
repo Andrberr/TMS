a16a7faa885e0548798a0a8797f222ae213876e2
BABASTAS728 <Aberezkin954@gmail.com>
2022-12-18 15:30:43 +0300

    stream fix3

--- app/src/main/java/com/example/homeworkarrays/lesson13/homework/StreamWork.java
+++ app/src/main/java/com/example/homeworkarrays/lesson13/homework/StreamWork.java
@@ -2,7 +2,6 @@ package com.example.homeworkarrays.lesson13.homework;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
